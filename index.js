6cbafed0b16475c81d54c41efcc2bd563ed1dd8a
ssagar <ssagar8186@gmail.com>
2023-09-11 11:16:25 +0530

    added vdome

---
+++ index.js
@@ -0,0 +1,26 @@
+function setfn1(){
+    console.log('hello')
+
+let element1= `<div> hey chitkara </div>`
+
+document.getElementById('container1').innerHTML = element1;
+// let a= new Date().toLocaleTimeString()
+
+}
+
+function setfn2(){
+    let element2 = React.createElement('div', null, "hey chitkara");
+ReactDOM.render(element2, document.getElementById('container2'))
+}
+
+setInterval(setfn1, 1000)
+
+
+
+
+
+
+
+
+
+
